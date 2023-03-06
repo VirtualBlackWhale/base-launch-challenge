ba53c88cca4b284ff17ddb9cc86f18adcd155b54
Victor Blackwhale <56893412+VirtualBlackWhale@users.noreply.github.com>
2023-03-06 21:22:22 +0100

    fix(index): add missing second sig

--- index.ts
+++ index.ts
@@ -42,12 +42,11 @@ const CONTRACT = new ethers.Contract(
 	abi,
 	WALLET
 );
-let RIDDLE_3_ETH_MESSAGE_HASH = '0x20a1626365cea00953c957fd02ddc4963990d404232d4e58acb66f46c59d9887';
 
 (async () => {
 	try {
 		console.log('challenge accepted');
-		
+
 		await (await CONTRACT.solveChallenge1('faucet')).wait();
 
 		const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('The Merge'));
@@ -55,18 +54,28 @@ let RIDDLE_3_ETH_MESSAGE_HASH = '0x20a1626365cea00953c957fd02ddc4963990d404232d4
 
 		await (await CONTRACT.solveChallenge2('The Merge', sig)).wait();
 
-		// any signature but must not have been used yet
-		const randomSig =
-			'0x7a768d10fe18b1563b69e8f138269d1c30646b553344b389cd27183cd92e4283699461494e48d7ed3559329c5ea869064808471e205ac31bb5d6e6107ac3aeef1b';
-		const recoverAddress = ethers.utils.recoverAddress(
-			RIDDLE_3_ETH_MESSAGE_HASH,
-			randomSig
+		const hashChallenge3 = ethers.utils.keccak256(
+			ethers.utils.toUtf8Bytes('EIP-4844')
+		);
+		const sigChallenge3 = await WALLET.signMessage(
+			ethers.utils.arrayify(hashChallenge3)
 		);
 
 		await (
-			await CONTRACT.solveChallenge3('EIP-4844', recoverAddress, randomSig)
+			await CONTRACT.solveChallenge3('EIP-4844', WALLET.address, sigChallenge3)
 		).wait();
-		
+
+		const sigChallenge3Compact =
+			ethers.utils.splitSignature(sigChallenge3).compact;
+
+		await (
+			await CONTRACT.solveChallenge3(
+				'EIP-4844',
+				WALLET.address,
+				sigChallenge3Compact
+			)
+		).wait();
+
 		console.log('challenge fulfilled');
 	} catch (error) {
 		console.error(error);
